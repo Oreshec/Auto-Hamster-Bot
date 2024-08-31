a7995990be104b8720dd09c8edf81c7fe8b0c22f
Oreshec <vlad.mumiay12@mail.ru>
2024-08-31 12:40:59 +0300

    Поворочал туда сюда. Был баг: Если ячейка была NaN, то она не проходила проверку на кд

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ import pandas as pd
 import conf
 import info_profile
 import upgrade
+import numpy as np
 
 save_to_excel = False
 UPGRADE = True
+
 
 def fetch_data():
     """Fetch data from the API and return as JSON."""
@@ -16,13 +18,14 @@ def fetch_data():
         "Authorization": f"{conf.authorization}"
     }
     response = requests.post(url_info, json="", headers=headers)
-    print('Получение информации о картах')
+    print(f'Получение информации о картах с {url_info}')
     print('Status: ', response.status_code)
     if response.status_code == 200:
         return response.json()
     else:
         print('Ошибка получения данных от API')
         return None
+
 
 def process_data(data):
     """Process the data into a DataFrame and filter it."""
@@ -36,6 +39,7 @@ def process_data(data):
         print('Ошибка в обработке данных:\n', traceback.format_exc())
         return None
 
+
 def save_to_excel_file(df):
     """Save the DataFrame to an Excel file."""
     try:
@@ -45,31 +49,37 @@ def save_to_excel_file(df):
     except Exception:
         print('Ошибка при сохранении в Excel:\n', traceback.format_exc())
 
+
 def perform_upgrade(df):
     """Attempt to upgrade cards based on current money."""
     try:
         index = df.index[:5].tolist()
         money = info_profile.get_info_money()
         for i in index:
-            price = df.at[i, 'price']
-            if money > price:
-                id_card = df.at[i, 'id']
-                print('Деньга на ', id_card, ' есть')
-                cooldown = df.at[i, 'cooldownSeconds']
-                if cooldown <= 0:
-                    print('Кд тоже нет покупаю')
+            cooldown = df.at[i, 'cooldownSeconds']
+            id_card = df.at[i, 'id']
+            if cooldown <= 0 or np.isnan(cooldown):
+                print('Кд на ', id_card, ' нет')
+                price = df.at[i, 'price']
+                if money > price:
+                    print('Деньга на ', id_card, ' есть\n')
                     upgrade.upgrade_card(id_card=id_card)
                 else:
-                    print(f'Кд {cooldown}\n')
-            else:
-                print(f'Деньга нет на {df.at[i, "id"]} стоимостью {price} монет. Сейчас деняк: {money}')
+                    print(f'Деньга нет на {df.at[i, "id"]} стоимостью {price} монет. Сейчас деняк: {money}\n')
+            elif cooldown > 0:
+                print(f'{id_card} в кд {cooldown}\n')
 
         min_cd = df['cooldownSeconds'][:5].min()
-        print('Сон на: ', min_cd)
-        time.sleep(min_cd)
-        main()
+        if min_cd == 0:
+            time.sleep(30)
+            main()
+        else:
+            print('Сон на: ', min_cd)
+            time.sleep(min_cd)
+            main()
     except Exception:
         print('Ошибка при обновлении карты:\n', traceback.format_exc())
+
 
 def main():
     data = fetch_data()
@@ -85,6 +95,7 @@ def main():
 
     if UPGRADE:
         perform_upgrade(df)
+
 
 if __name__ == "__main__":
     main()
